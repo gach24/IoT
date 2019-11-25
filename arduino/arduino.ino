d63a8940f37e6564f1812a1bd3256db4aadb0173
Unknown <hevia.german@gmail.com>
2019-11-25 11:00:24 +0100

    Modificadas conexiones

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,7 +5,8 @@
 #define DHTTYPE DHT22
 
 const int DHTPin = 4; 
-const String server = "10.140.76.179"; // String server = "sonofe.ddns.net";
+const String server = "52.3.232.93"; // String server = "sonofe.ddns.net";
+// const String server = "10.140.76.179"; // String server = "sonofe.ddns.net";
 // const String server = "127.0.0.1"; // String server = "sonofe.ddns.net";
 SoftwareSerial BT1(3, 2); // RX | TX
 DHT dht(DHTPin, DHTTYPE);
@@ -79,15 +80,15 @@ void loop() {
    */
   Serial.println("****** CONEXIÓN TCP CON EL SERVIDOR *******");
   BT1.println("AT+CIPSTART=\"TCP\",\"" + server + "\",80"); 
-  // respuesta();
+  respuesta();
   delay(1000);
 
   
   /*
    * Componemos mensaje para enviar a server 
    */
-  String peticionHTTP = "GET /temperatures/insert.php?temperature=" + String(temperatura) + "&humidity=" + String(humedad) + " HTTP/1.1\r\n";
-  peticionHTTP = peticionHTTP + "Host: 10.140.76.179\r\n\r\n"; // peticionHTTP = peticionHTTP + "Host: sonofe.ddns.net\r\n\r\n";
+  String peticionHTTP = "GET /insert.php?temperature=" + String(temperatura) + "&humidity=" + String(humedad) + " HTTP/1.1\r\n";
+  peticionHTTP = peticionHTTP + "Host: 52.3.232.93\r\n\r\n"; // peticionHTTP = peticionHTTP + "Host: sonofe.ddns.net\r\n\r\n";
 
   /**
    * Wifi: Envió tamaño del comando
@@ -124,22 +125,7 @@ void loop() {
 }
 
 
-void printTime()
-{
-  time_t t = now();
-  Serial.print(day(t));
-  Serial.print(+ "/") ;
-  Serial.print(month(t));
-  Serial.print(+ "/") ;
-  Serial.print(year(t)); 
-  Serial.print( " ") ;
-  Serial.print(hour(t));  
-  Serial.print(+ ":") ;
-  Serial.print(minute(t));
-  Serial.print(":") ;
-  Serial.println(second(t));
-  delay(1000);  
-}
+
 
 void respuesta() {
   String respuesta = "";
