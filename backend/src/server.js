c5883a98870744141ea1e16b406626ee0c53d474
Germán Carreño <germanach@educastur.org>
2019-12-04 08:53:20 +0000

    Añadidos comentario a la función listen
    En el comentario se indica tambien una manera alternativa de mostrar el log de la aplicación

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,5 +12,10 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
+
+/**
+ * Listen express on 0.0.0.0 host and 8080 port
+ * app.listen(PORT, HOST, () => console.log('Express listening on port 3000 ...'));
+ */
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
