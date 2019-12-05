eb3e1f88b48c9d4f43ffff373a1eb582440a5d3b
Germán Carreño <germanach@educastur.org>
2019-12-05 13:38:05 +0000

    Añadida conexión de express a mongodb mediante mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,28 @@
 'use strict';
 
-const express = require('express');
-
 // Constants
-const PORT = 8080;
+const PORT        = 8080;
 const HOST = '0.0.0.0';
+
+
+
+
+const express   = require('express');
+const mongoose  = require('mongoose');
+
+
+
+
+
+/**
+mongoose.connection.openUri('mongodb://dev:dev@db:27017/temperatures', (err, res) => {
+  if (err)
+    throw err;
+  console.log('Mongo running on: \x1b[42m%s\x1b[0m', 'mongodb://db/temperatures:27012')
+});
+ */
+
+
 
 // App
 const app = express();
@@ -15,10 +33,21 @@ app.get('/', (req, res, next) => {
   });
 });
 
+setTimeout(function() {
+  mongoose.connect('mongodb://dev:dev@db:27017/temperatures', {useNewUrlParser: true})
+    .then(
+      () => {
+        console.log('Running mongodb on: \x1b[42m%s\x1b[0m', 'mongodb://dev:dev@db:27017/temperatures')
+        /**
+          * Listen express on 0.0.0.0 host and 8080 port
+          * app.listen(PORT, HOST, () => console.log('Express listening on port 3000 ...'));
+          */
+        app.listen(PORT, HOST);
+        console.log('Running express on: \x1b[42m%s\x1b[0m', `http://${HOST}:${PORT}`);
+      },
+      err => { throw err }
+  );
 
-/**
- * Listen express on 0.0.0.0 host and 8080 port
- * app.listen(PORT, HOST, () => console.log('Express listening on port 3000 ...'));
- */
-app.listen(PORT, HOST);
-console.log('Running on: \x1b[42m%s\x1b[0m', `http://${HOST}:${PORT}`);
+}, 10000);
+
+
