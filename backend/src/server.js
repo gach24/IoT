3031012613211e527280ba158a43e575ae4faa2c
Germán Carreño <germanach@educastur.org>
2019-12-09 00:23:49 +0000

    Modificado mensaje de log, cuando ponemos a la escucha express

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@
 
 // Constants
 const PORT        = 8080;
-const HOST = '0.0.0.0';
+const HOST        = '0.0.0.0';
 
 
 
@@ -38,16 +38,13 @@ setTimeout(function() {
     .then(
       () => {
         console.log('Running mongodb on: \x1b[42m%s\x1b[0m', 'mongodb://dev:dev@db:27017/temperatures')
-        /**
-          * Listen express on 0.0.0.0 host and 8080 port
-          * app.listen(PORT, HOST, () => console.log('Express listening on port 3000 ...'));
-          */
-        app.listen(PORT, HOST);
-        console.log('Running express on: \x1b[42m%s\x1b[0m', `http://${HOST}:${PORT}`);
+      
+        app.listen(PORT, HOST, () => {
+          console.log('Running express on: \x1b[42m%s\x1b[0m', `http://${HOST}:${PORT}`);
+        });
       },
       err => { throw err }
   );
-
 }, 10000);
 
 
