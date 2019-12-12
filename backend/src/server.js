c70e70002f48836edf9e396c6464ea95ac9514b7
Germán Carreño <germanach@educastur.org>
2019-12-12 14:32:08 +0000

    Añadido y configurado paquete cors
    Se añade este paquete para permitir llamadas desde un cliente javascript
    de otro dominio

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,10 @@ const HOST_MONGO          = 'db';
 const DB_MONGO            = 'datadb'
 
 
-
-const express   = require('express');
 const mongoose  = require('mongoose');
+const cors      = require('cors');
+const express   = require('express');
+
 
 var dataSchema = new mongoose.Schema({
   temperature: { 
@@ -31,8 +32,24 @@ var dataSchema = new mongoose.Schema({
 var Measure = mongoose.model('Measure', dataSchema);
 
 
+
+
 // App
-const app = express();
+var app = express();
+
+/*
+ * Para permitir llamadas javascript a otros dominios
+ * Sin embargo utilizamos el paquete de node cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+ */
+
+app.use(cors());
 
 app.get('/insert', (req, res, next) => {
 
@@ -44,22 +61,15 @@ app.get('/insert', (req, res, next) => {
   measure.save()
     .then(newMeasure => res.status(200).json({ok: true, new: newMeasure}))
     .catch(err => res.status(500).json({ok: false, message: "No se ha podido insertar en la base de datos" }));
-
-
-  /*
-  res.status(200).json({
-    ok: true,
-    msg: `La temperatura es ${temperature} y la humedad es ${humitity}`
-  });
-  */
-
 });
 
 
-app.get('/', (req, res, next) => {
+app.get('/',  (req, res, next) => {
 
   Measure.find({}).exec()
-    .then(data => res.status(200).json({ok: true, measures: data }))
+    .then(data => {
+      res.status(200).json({ok: true, measures: data })
+    })
     .catch(err => res.status(500).json({ok: false, message: "No se ha podido conectar a la base de datos" }));
 
 });
